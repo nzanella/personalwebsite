f39d522f3bb856c1d6bc34570b94f585638be9d4
Neil Zanella <nzanella@gmail.com>
2021-02-20 14:42:18 +0100

    added file that illustrates problem with treeNode v7 php file.

---
+++ examplecode/includes/check.php
@@ -0,0 +1,43 @@
+<?php
+
+class Foo {
+
+  public static function greet($name) {
+
+    echo "Hi $name. This is Foo.\n";
+
+  }
+
+}
+
+class Bar {
+
+  public static function greet($name){
+
+    echo "Hi $name. This is Bar.\n";
+
+  }
+
+} 
+
+class Baz extends Bar {
+
+  // changing number of arguments in static function of same name in subclass
+
+  // generates: Fatal error: Declaration of Baz::greet($name, $name2) must be compatible with Bar::greet($name)
+  public static function greet($name, $name2){
+
+    echo "Hi $name and $name2. This is Baz.\n";
+
+  }
+
+}
+
+
+Foo::greet("David");
+
+Bar::Greet("Anne");
+
+Baz::Greet("Bob");
+
+?>
